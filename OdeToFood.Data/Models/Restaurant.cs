e30525ae5800c660fb336cb009e715db02775b12
Marius Lungu <marius_lng@yahoo.com>
2021-10-24 20:26:28 +0300

    Add project files.

---
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OdeToFood.Data.Models
+{
+    public class Restaurant
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public CuisineType Cuising { get; set; }
+    }
+}
---
+++ OdeToFood.Data/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> Restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            this.Restaurants = new List<Restaurant>()
+            {
+                new Restaurant{ID = 1, Name = "Scott's Pizza", Cuising = CuisineType.Italian},
+                new Restaurant{ID = 2, Name = "Tersiguels", Cuising = CuisineType.French},
+                new Restaurant{ID = 3, Name = "Mango Grove", Cuising = CuisineType.Indian }
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return this.Restaurants.OrderBy(r => r.Name);
+        }
+    }
+}
